5c1705149516be698d55946ac7948f99e6214d25
IceCodeNew <32576256+IceCodeNew@users.noreply.github.com>
2019-03-11 22:55:24 +0800

    Initial commit

---
+++ (For Git Bash) FuckWindowsSpy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+read -rp "Where to download the rules file? "
+REPLY=${REPLY//\\/\/}
+root_letter=`echo ${REPLY:0:1} | tr '[:upper:]' '[:lower:]'`
+REPLY='/'$root_letter'/'${REPLY:3}
+echo "The specified dir is: $REPLY"
+unset root_letter
+cd $REPLY
+
+rm spy.txt extra.txt
+curl -O https://raw.githubusercontent.com/crazy-max/WindowsSpyBlocker/master/data/dnscrypt/extra.txt
+curl -O https://raw.githubusercontent.com/crazy-max/WindowsSpyBlocker/master/data/dnscrypt/spy.txt
+
+python3 fws.py
